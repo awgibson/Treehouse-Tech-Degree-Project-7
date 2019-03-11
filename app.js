4cbde0a63ed04db874965ee0baee2255e1fa5361
awgibson <34604592+awgibson@users.noreply.github.com>
2019-03-11 17:21:43 -0500

    basic server created

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const port = 5000;
+
+const app = express();
+
+//Use express' body parser
+app.use(express.urlencoded({ extended: false }));
+
+//Set the static route
+app.use('/static', express.static('public'));
+
+//Set pug as the view engine
+app.set('view engine', 'pug');
+
+//Route for the root
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+//Server
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
